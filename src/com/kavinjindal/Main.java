68a652e236b1b5b662c30d3e9f971924310abf48
kavinjindal <kavinsjindal@gmail.com>
2021-10-29 20:40:53 +0530

    Initial commit

---
+++ src/com/kavinjindal/Main.java
@@ -0,0 +1,35 @@
+package com.kavinjindal;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("FizzBuzz by KJ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your number >> ");
+        int UserInput = scanner.nextInt();
+
+
+
+        if (UserInput % 5 == 0 && !(UserInput % 3 == 0)){
+            System.out.println("Fizz");
+        }
+        else if (UserInput % 3 == 0 && !(UserInput % 5 == 0)){
+            System.out.println("Buzz");
+
+        }
+        else if (UserInput % 5 == 0 && UserInput % 3 == 0){
+            System.out.println("FizzBuzz");
+        }
+        else
+            System.out.println(UserInput);
+        }
+
+
+
+
+        }
+
+
